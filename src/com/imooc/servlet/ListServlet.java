a21903a99542b68b2742aafb7725c86d4253f02d
zystar <1803999267@qq.com>
2017-01-07 13:08:10 +0800

    界面展示

---
+++ src/com/imooc/servlet/ListServlet.java
@@ -0,0 +1,63 @@
+package com.imooc.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.imooc.bean.Message;
+
+/**
+ * 
+ * 列表页面初始化控制
+ *
+ */
+@SuppressWarnings("serial")
+public class ListServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/micro_message", "root", "1234");
+			String sql = "select ID,COMMAND,DESCRIPTION,CONTENT from MESSAGE";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			List<Message> messageList = new ArrayList<Message>();
+			while(rs.next()){
+				Message message = new Message();
+				messageList.add(message);
+				message.setId(rs.getString("ID"));
+				message.setCommand(rs.getString("COMMAND"));
+				message.setDescription(rs.getString("DESCRIPTION"));
+				message.setContent(rs.getString("CONTENT"));
+			}
+			req.setAttribute("messageList", messageList);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		req.getRequestDispatcher("/WEB-INF/jsp/back/list.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doGet(req, resp);
+	}
+	
+}
